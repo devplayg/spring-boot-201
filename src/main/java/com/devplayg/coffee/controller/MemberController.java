aa17b15061bd48db7b83039eaab7ab957fa6fe04
devplayg <devplayg@gmail.com>
2019-05-17 16:28:59 +0900

    added member repo added audit and member template

--- src/main/java/com/devplayg/coffee/controller/MemberController.java
+++ src/main/java/com/devplayg/coffee/controller/MemberController.java
@@ -1,4 +1,19 @@
 package com.devplayg.coffee.controller;
 
+import com.devplayg.coffee.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("member")
 public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @GetMapping
+    public String get() {
+        return "member/member";
+    }
 }
--- src/main/java/com/devplayg/coffee/repository/MemberRepository.java
+++ src/main/java/com/devplayg/coffee/repository/MemberRepository.java
@@ -1,4 +1,7 @@
 package com.devplayg.coffee.repository;
 
-public interface MemberRepository {
+import com.devplayg.coffee.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
